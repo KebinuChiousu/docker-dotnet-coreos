57751f884de7047e12d0705e16b5b3c757c15358
Kevin Meredith <kevin@meredithkm.info>
2016-11-13 11:55:05 -0500

    Add project files.

---
+++ src/docker-dotnet-coreos/Models/ServerConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace docker_dotnet_coreos
+{
+  public class ServerConfig
+  {
+    public int Id { get; set; }
+    public string MAC { get; set; }
+    public string Name { get; set; }
+
+
+  }
+}
