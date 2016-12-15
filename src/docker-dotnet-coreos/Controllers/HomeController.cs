c858e558d5cfe6ae6602b8bb2c7bced30dac0988
Kevin Meredith <kevin@meredithkm.info>
2016-12-14 20:16:29 -0800

    Reorganize Code. Update HomeController with defaults.

--- src/docker-dotnet-coreos/Controllers/HomeController.cs
+++ src/docker-dotnet-coreos/Controllers/HomeController.cs
@@ -30,12 +30,20 @@ namespace docker_dotnet_coreos.Controllers
     public IActionResult CloudConfigYml(string mac)
     {
       var cloud_config = new CloudConfig();
+
+      var token = "<token>";
       
-      cloud_config.coreos.etcd2.discovery = "https://discovery.etcd.io/<token>";
+      cloud_config.coreos.etcd2.discovery = "https://discovery.etcd.io/" + token;
       cloud_config.coreos.etcd2.advertise_dash_client_dash_urls = "http://$public_ipv4:2379";
       cloud_config.coreos.etcd2.initial_dash_advertise_dash_peer_dash_urls = "http://$private_ipv4:2380";
       cloud_config.coreos.etcd2.listen_dash_client_dash_urls = "http://0.0.0.0:2379,http://0.0.0.0:4001";
       cloud_config.coreos.etcd2.listen_dash_peer_dash_urls = "http://$private_ipv4:2380,http://$private_ipv4:7001";
+
+      cloud_config.InitSshAuthKey();
+
+      var ssh_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAgEAxrht21lxTFLncowgatSVn/O8LftHB6prAqsdod/9K7c/wFw94/8MO2GhC06nt00jroq76RDDl/f/9H4eZapoC7umyGdUW4bzs/ultAwH8+aWGfOag7ic0Hwv/Fl3YIPmFC/dot6Pc3rY9whwR4QtV9sDlfDPgskVfTWAID987d6KYYl4QxdlKopuWkXFZ5LrMWuz/oaVu6LHSHS4sCx1DfNYEw+SHBGk6loEEHkHdZr3UvdnYSPagoE6bMm+cngSY0naWo57006ffDjdG8Oh6L4QmydpjjmRGQy7Wiz9FHDMTKjp8lDVxBg0e8j8WpvH/NyHU0ylgWHNi7toN/nqzfEnZ2V/X2EGaFX6pkdliPUCfkKsn4dSagWmDFN4rsfTUFmK9/yMbRbhyaT6/yQWqXqzVBPaFh/EXcONeYEr3jcTzFLHCae0UqHDGSDi2qMp4ixIHC0MuBkMJm0VQEECyVmBcTQ2q0ThNvofQTnhGnTvoi3wx9+TBH4dZAuhTa4ndEQSsogNmSCAvrCpMhSrdIrai2fUuOGUZt38BndqHLR54rTcBa/5iI4bjg+Rd7eQxseJT/zhQEmBdlALTCEjMLOvSbmkEX50vehWCCoR/RIySCU1/gdagAuCN8lZbD3qOIFzRXhamLjL0iD5MFqbGTh/hG2cBCvCqyHWpUIO3Ms= meredithk_ssh_rsa_4096";
+              
+      cloud_config.ssh_authorized_keys.Add(ssh_key);
 
       cloud_config.InitUsers();
 
@@ -49,6 +57,10 @@ namespace docker_dotnet_coreos.Controllers
       user1.groups.Add("sudo");
       user1.groups.Add("docker");
 
+      user1.InitSshAuthKey();
+
+      user1.ssh_authorized_keys.Add(ssh_key);
+
       cloud_config.users.Add(user1);
 
       var user2 = new CloudConfigUser();
@@ -61,6 +73,10 @@ namespace docker_dotnet_coreos.Controllers
       user2.groups.Add("sudo");
       user2.groups.Add("docker");
 
+      user2.InitSshAuthKey();
+
+      user2.ssh_authorized_keys.Add(ssh_key);
+
       cloud_config.users.Add(user2);
 
       cloud_config.hostname = "coreos01";
@@ -71,16 +87,14 @@ namespace docker_dotnet_coreos.Controllers
       var serializer = new Serializer();
       serializer.Serialize(strWriter, cloud_config);
 
-      string remoteIpAddress = HttpContext.Connection.RemoteIpAddress.ToString();
+      output = mac;
+      output += "\n\n";
 
-      //output = mac;
-      //output += "\n\n";
-
-      output = "#cloud-config\n\n";
-      output += strWriter.ToString(). 
+      output += "#cloud-config\n\n";
+      output += strWriter.ToString().
                 Replace("_dash_", "-").
-                Replace("$public_ipv4","74.100.8.20").
-                Replace("$private_ipv4",remoteIpAddress);
+                Replace("$public_ipv4", "10.0.75.101").
+                Replace("$private_ipv4","10.0.75.101");
           
       return this.Content(output, "text/yml", Encoding.UTF8);
 
---
+++ src/docker-dotnet-coreos/Models/database/Model.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace docker_dotnet_coreos.database
+{
+  public class ServerContext : DbContext
+  {
+    public DbSet<User> Users { get; set; }
+    public DbSet<Group> Groups { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlite("Filename=./App_Data/ServerConfig.sqlite");
+    }
+  }
+
+  public class User
+  {
+    public int UserId { get; set; }
+    public string UserName { get; set; }
+    public string gecos { get; set; }
+    public string passwd { get; set; }
+    public string homedir { get; set; }
+    public Boolean no_create_home { get; set; }
+    public string primary_dash_group { get; set; }
+    public List<Group> groups { get; set; }
+    public Boolean no_user_group { get; set; }
+    public List<string> ssh_authorized_keys { get; set; }
+    public string system { get; set; }
+    public Boolean no_log_init { get; set; }
+    public string shell { get; set; }
+    public string Url { get; set; }
+        
+  }
+
+  public class Group
+  {
+    public int GroupID { get; set; }
+    public int GroupName { get; set; }
+
+    public int UserID { get; set; }
+    public User User { get; set; }
+  }
+    
+}
